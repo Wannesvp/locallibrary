f40c9a81eb8349f00a852bb72716e0058e35d8d8
Wannes Van Praet <wannes.van.praet@atlascopco.com>
2022-11-22 10:13:23 +0100

    added files and changes for deployment

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +134,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # code from " https://stackoverflow.com/questions/14999134/how-to-pass-a-message-from-httpresponseredirect-in-django "
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
